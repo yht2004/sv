fcd148437ede8a670fd8d73adbf2b5dff62773be
yht2004 <1921959159@qq.com>
2019-10-24 16:04:56 +0800

    continue1

--- src/main/java/com/data2/Data2Application.java
+++ src/main/java/com/data2/Data2Application.java
@@ -1,9 +1,11 @@
 package com.data2;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.data2.project.system.*.mapper")
 public class Data2Application {
 
     public static void main(String[] args) {
--- src/test/java/com/data2/Data2ApplicationTests.java
+++ src/test/java/com/data2/Data2ApplicationTests.java
@@ -2,24 +2,43 @@ package com.data2;
 
 
 
+import com.data2.project.system.user.entity.User;
+import com.data2.project.system.user.mapper.UserMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Data2ApplicationTests {
 
-
+    @Autowired
+    UserMapper userMapper;
 
     @Test
-    public void contextLoads() {
-
+    public void selectAllUser() {
+        List<User> users = userMapper.selectAllUser();
+        System.out.println(users);
+        for (User user : users){
+            System.out.println(user.getUserName());
+        }
     }
 
+    @Test
+    public void selectUserByUserId() {
+        User user = userMapper.selectUserByUserId(52);
+        if (user ==null){
+            System.out.println("无此用户");
+        }else {
+            System.out.println(user.getUserName());
+        }
+
+    }
 
 
 }
